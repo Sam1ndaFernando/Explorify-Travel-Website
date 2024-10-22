365de017ec196cb4a0f4c7c74a6305a7950d6efb
Saami <uthpala2001@gmail.com>
2024-10-22 19:24:55 +0530

    blur-header error fix

--- js/script.js
+++ js/script.js
@@ -25,12 +25,12 @@ const linkAction = () =>{
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-const blurHeader = () =>{
-    const header = document.getElementById('header')
-    this.scrollY >= 50 ? header.classList.add('blur-header')
-        : header.classList.remove('blur-header')
-}
-window.addEventListener('scroll', blurHeader)
+const blurHeader = () => {
+    const header = document.getElementById('header');
+    window.scrollY >= -5 ? header.classList.add('blur-header') : header.classList.remove('blur-header');
+};
+window.addEventListener('scroll', blurHeader);
+
 
 const scrollUp = () =>{
     const scrollUp = document.getElementById('scroll-up')
