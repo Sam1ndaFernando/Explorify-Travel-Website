fbc66d085f8f56e484ef69939119ef5934aa9b1d
Saami <uthpala2001@gmail.com>
2024-10-22 11:07:22 +0530

    js Updated

--- js/script.js
+++ js/script.js
@@ -29,7 +29,6 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 /*=============== Blur Header ===============*/
 const blurHeader = () =>{
     const header = document.getElementById('header')
-    // Add a class if the bottom offset is greater than 50 of the viewport
     this.scrollY >= 50 ? header.classList.add('blur-header')
         : header.classList.remove('blur-header')
 }
@@ -38,7 +37,6 @@ window.addEventListener('scroll', blurHeader)
 /*=============== Scroll Sections ===============*/
 const scrollUp = () =>{
     const scrollUp = document.getElementById('scroll-up')
-    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
     this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
         : scrollUp.classList.remove('show-scroll')
 }
@@ -55,7 +53,7 @@ const scrollActive = () =>{
         const sectionHeight = current.offsetHeight,
             sectionTop = current.offsetTop - 58,
             sectionId = current.getAttribute('id'),
-            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+            sectionsClass = document.querySelector('.navMenu a[href*=' + sectionId + ']')
 
         if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
             sectionsClass.classList.add('active-link')
@@ -75,8 +73,8 @@ const sr = ScrollReveal({
     delay: 400,
 })
 
-sr.reveal('.home__data, .explore__data, .explore__user, .footer__container')
-sr.reveal('.home__card', {delay: 600, distance: '100px', interval: 100})
-sr.reveal('.about__data, .join__image', {origin: 'right'})
-sr.reveal('.about__image, .join__data', {origin: 'left'})
-sr.reveal('.popular__card', {interval: '200'})
+sr.reveal('.homeData, .exploreData, .exploreUser, .footerContainer')
+sr.reveal('.homeCard', {delay: 600, distance: '100px', interval: 100})
+sr.reveal('.aboutData, .joinImage', {origin: 'right'})
+sr.reveal('.aboutImage, .joinData', {origin: 'left'})
+sr.reveal('.popularCard', {interval: '200'})
